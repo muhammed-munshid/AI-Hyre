7ba7674d13f668c17cb5a4c953d1b81e6d72f3d7
Munshid <muhammadmunshid200@gmail.com>
2023-10-07 13:13:43 +0530

    small change updated

--- controller/chat-controller.js
+++ controller/chat-controller.js
@@ -1,9 +1,35 @@
 const crypto = require('crypto')
 const chatModel = require("../model/chatModel");
+const candidateModel = require('../model/candidateModel');
 
 module.exports = {
 
-    messages: async (req, res) => {
+    addChat: async (req, res) => {
+        try {
+            const { candidate, recruiter, job } = req.body;
+
+            // Check if a chat already exists between the two candidaate and post
+            const existingChat = await chatModel.findOne({ candidate, recruiter, job });
+            console.log('exist:', existingChat);
+
+            if (existingChat) {
+                // A chat already exists, so return it instead of creating a new one
+                res.status(200).send(existingChat);
+                return;
+            }
+
+            // Create a new chat if one doesn't already exist
+            const chat = new chatModel({ candidate, recruiter, job });
+            await chat.save();
+            res.status(201).send(chat);
+        } catch (err) {
+            console.log(err);
+            res.status(400).send(err);
+        }
+    },
+
+
+    viewMessageById: async (req, res) => {
         try {
             const chat = await chatModel.findById(req.params.id).populate('candidate recruiter messages.sender messages.receiver', 'name email').maxTimeMS(20000)
             console.log('chat', chat.messages);
@@ -18,8 +44,8 @@ module.exports = {
     v1Chat: async (req, res) => {
         try {
             const { id } = req.params;
-            const chats = await chatModel.find({ candidate: id })
-                .populate('candidate', 'username')
+            const chats = await chatModel.find({ candidate: id, recruiter:id })
+                .populate('candidate', 'recruiter')
                 .populate({
                     path: 'messages',
                     options: { sort: { time: -1 }, limit: 1 },
@@ -46,66 +72,77 @@ module.exports = {
     //same as above
     viewChat: async (req, res) => {
         try {
-            const { id } = req.params;
-            const chats = await chatModel.find({ candidate: id })
-                .populate({ path: 'candidate', select: 'name', transform: 'name' })
-                .populate({
+            const id = req.params.id
+            console.log(id);
+            const candidate = await candidateModel.findById(id)
+            const recruiter = await recruiterModel.findById(id)
+            console.log('recruiter:',recruiter);
+            if (candidate) {                
+                const chats = await chatModel.find({ candidate: id })
+                  .populate({ path: 'candidate', select: 'name', transform: 'name' })
+                  .populate({
                     path: 'messages',
                     options: { sort: { time: -1 }, limit: 1 },
                     populate: {
-                        path: 'sender receiver',
-                        select: 'name'
+                      path: 'sender receiver',
+                      select: 'name'
                     }
-                })
-                .exec();
-            const chatList = await Promise.all(chats.map(async chat => {
-                const unreadCount = await chatModel.countDocuments({
+                  })
+                  .exec();
+                const chatList = await Promise.all(chats.map(async chat => {
+                  const unreadCount = await chatModel.countDocuments({
                     _id: chat._id,
                     'messages.status': 'sent',
                     'messages.sender': { $ne: id }
-                });
-                return {
+                  });
+                  return {
                     id: chat._id,
                     candidate: chat.candidate,
                     post: chat.post,
                     lastMessage: chat.messages[0]?.message,
                     lastMessageTime: chat.messages[0]?.time,
                     unreadCount
-                };
-            }));
-            res.send(chatList);
-        } catch (err) {
-            console.log(err);
-            res.status(400).send(err);
-        }
-    },
-
-    createChat: async (req, res) => {
-        try {
-            const { candidate, recruiter, job } = req.body;
-
-            // Check if a chat already exists between the two candidaate and post
-            const existingChat = await chatModel.findOne({ candidate, recruiter, job });
-            console.log('exist:', existingChat);
-
-            if (existingChat) {
-                // A chat already exists, so return it instead of creating a new one
-                res.status(200).send(existingChat);
-                return;
+                  };
+                }));
+                res.send(chatList);
+            } else if (recruiter) {
+                const chats = await chatModel.find({ recruiter: id })
+                  .populate({ path: 'recruiter', select: 'name', transform: 'name' })
+                  .populate({
+                    path: 'messages',
+                    options: { sort: { time: -1 }, limit: 1 },
+                    populate: {
+                      path: 'sender receiver',
+                      select: 'name'
+                    }
+                  })
+                  .exec();
+                const chatList = await Promise.all(chats.map(async chat => {
+                  const unreadCount = await chatModel.countDocuments({
+                    _id: chat._id,
+                    'messages.status': 'sent',
+                    'messages.sender': { $ne: id }
+                  });
+                  return {
+                    id: chat._id,
+                    recruiter: chat.recruiter,
+                    post: chat.post,
+                    lastMessage: chat.messages[0]?.message,
+                    lastMessageTime: chat.messages[0]?.time,
+                    unreadCount
+                  };
+                }));
+                res.send(chatList);
+            } else {
+                console.log('what is this?');
             }
-
-            // Create a new chat if one doesn't already exist
-            const chat = new chatModel({ candidate, recruiter, job });
-            await chat.save();
-            res.status(201).send(chat);
         } catch (err) {
             console.log(err);
             res.status(400).send(err);
         }
     },
 
-    //Adding a message into a chat:
-    message: async (req, res) => {
+    addMessage: async (req, res) => {
         try {
             const chat = await chatModel.findById(req.params.id);
 
--- routes/chat.js
+++ routes/chat.js
@@ -5,12 +5,12 @@ const router = express.Router();
 
 passport.initialize();
 
-router.get('/:id/messages', chatController.messages)
+router.get('/:id/messages', chatController.viewMessageById)
 router.get('/v1/:id/user', chatController.v1Chat)
 router.get('/:id/user', chatController.viewChat)
 
-router.post('/create', chatController.createChat)
-router.post('/:id/messages', chatController.message)
+router.post('/create', chatController.addChat)
+router.post('/:id/add-message', chatController.addMessage)
 
 router.patch('/:chatId/messages/:messageId', chatController.updateChat)
 
