d313bc4ecb2a9a2653792552b19f24f51922b045
Munshid <muhammadmunshid200@gmail.com>
2023-10-15 22:38:48 +0530

    small update changed

--- controller/chat-controller.js
+++ controller/chat-controller.js
@@ -10,13 +10,13 @@ module.exports = {
 
             // Check if a chat already exists between the two users and post
             const existingChat = await chatModel.findOne({ users: { $all: users }, job: job });
-        
+
             if (existingChat) {
-              // A chat already exists, so return it instead of creating a new one
-              res.status(200).send(existingChat);
-              return;
+                // A chat already exists, so return it instead of creating a new one
+                res.status(200).send(existingChat);
+                return;
             }
-        
+
             // Create a new chat if one doesn't already exist
             const chat = new chatModel({ users, job });
             await chat.save();
@@ -42,7 +42,7 @@ module.exports = {
     v1Chat: async (req, res) => {
         try {
             const { id } = req.params;
-            const chats = await chatModel.find({ candidate: id, recruiter:id })
+            const chats = await chatModel.find({ candidate: id, recruiter: id })
                 .populate('candidate', 'recruiter')
                 .populate({
                     path: 'messages',
@@ -71,34 +71,34 @@ module.exports = {
     viewChat: async (req, res) => {
         try {
             const id = req.params.id
-            const users = await User.findById(id)          
-                const chats = await chatModel.find({ users: id })
-                  .populate({ path: 'users', select: 'name', transform: 'name' })
-                  .populate({
+            const users = await User.findById(id)
+            const chats = await chatModel.find({ users: users })
+                .populate({ path: 'users', select: 'name', transform: 'name' })
+                .populate({
                     path: 'messages',
                     options: { sort: { time: -1 }, limit: 1 },
                     populate: {
-                      path: 'sender receiver',
-                      select: 'name'
+                        path: 'sender receiver',
+                        select: 'name'
                     }
-                  })
-                  .exec();
-                const chatList = await Promise.all(chats.map(async chat => {
-                  const unreadCount = await chatModel.countDocuments({
+                })
+                .exec();
+            const chatList = await Promise.all(chats.map(async chat => {
+                const unreadCount = await chatModel.countDocuments({
                     _id: chat._id,
                     'messages.status': 'sent',
                     'messages.sender': { $ne: id }
-                  });
-                  return {
+                });
+                return {
                     id: chat._id,
                     users: chat.users,
                     post: chat.post,
                     lastMessage: chat.messages[0]?.message,
                     lastMessageTime: chat.messages[0]?.time,
                     unreadCount
-                  };
-                }));
-                res.send(chatList);
+                };
+            }));
+            res.send(chatList);
         } catch (err) {
             console.log(err);
             res.status(500).send(err);
